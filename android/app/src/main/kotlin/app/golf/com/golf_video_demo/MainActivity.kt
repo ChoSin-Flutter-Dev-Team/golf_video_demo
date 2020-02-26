924f72af88d149a57a0adf66fe6ef4219e6d959c
Chayon Sinha <chayonrlz08@gmail.com>
2020-02-26 11:13:38 +0400

    Initial commit

---
+++ android/app/src/main/kotlin/app/golf/com/golf_video_demo/MainActivity.kt
@@ -0,0 +1,12 @@
+package app.golf.com.golf_video_demo
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
---
+++ lib/draw_app.dart
@@ -0,0 +1,385 @@
+import 'dart:io';
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_colorpicker/flutter_colorpicker.dart';
+import 'package:video_player/video_player.dart';
+import 'package:image_picker/image_picker.dart';
+
+class Draw extends StatefulWidget {
+  @override
+  _DrawState createState() => _DrawState();
+}
+
+class _DrawState extends State<Draw> {
+  Future<void> _initializeVideoPlayerFuture;
+  VideoPlayerController _controller;
+  File videoFile;
+
+  Color selectedColor = Colors.black;
+  Color pickerColor = Colors.black;
+  double strokeWidth = 3.0;
+  List<DrawingPoints> points = List();
+  bool showBottomList = false;
+  double opacity = 1.0;
+  StrokeCap strokeCap = (Platform.isAndroid) ? StrokeCap.butt : StrokeCap.round;
+  SelectedMode selectedMode = SelectedMode.StrokeWidth;
+
+  Offset startPoint, endPoint;
+
+  List<Color> colors = [
+    Colors.red,
+    Colors.green,
+    Colors.blue,
+    Colors.amber,
+    Colors.black
+  ];
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Container(
+            padding: const EdgeInsets.only(left: 8.0, right: 8.0),
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(50.0),
+                color: Colors.greenAccent),
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      IconButton(
+                          icon: Icon(Icons.album),
+                          onPressed: () {
+                            setState(() {
+                              if (selectedMode == SelectedMode.StrokeWidth)
+                                showBottomList = !showBottomList;
+                              selectedMode = SelectedMode.StrokeWidth;
+                            });
+                          }),
+                      IconButton(
+                          icon: Icon(Icons.opacity),
+                          onPressed: () {
+                            setState(() {
+                              if (selectedMode == SelectedMode.Opacity)
+                                showBottomList = !showBottomList;
+                              selectedMode = SelectedMode.Opacity;
+                            });
+                          }),
+                      IconButton(
+                          icon: Icon(Icons.color_lens),
+                          onPressed: () {
+                            setState(() {
+                              if (selectedMode == SelectedMode.Color)
+                                showBottomList = !showBottomList;
+                              selectedMode = SelectedMode.Color;
+                            });
+                          }),
+                      IconButton(
+                          icon: Icon(Icons.clear),
+                          onPressed: () {
+                            setState(() {
+                              showBottomList = false;
+                              points.clear();
+                            });
+                          }),
+                    ],
+                  ),
+                  Visibility(
+                    child: (selectedMode == SelectedMode.Color)
+                        ? Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: getColorList(),
+                          )
+                        : Slider(
+                            value: (selectedMode == SelectedMode.StrokeWidth)
+                                ? strokeWidth
+                                : opacity,
+                            max: (selectedMode == SelectedMode.StrokeWidth)
+                                ? 50.0
+                                : 1.0,
+                            min: 0.0,
+                            onChanged: (val) {
+                              setState(() {
+                                if (selectedMode == SelectedMode.StrokeWidth)
+                                  strokeWidth = val;
+                                else
+                                  opacity = val;
+                              });
+                            }),
+                    visible: showBottomList,
+                  ),
+                ],
+              ),
+            )),
+      ),
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        child: Stack(
+          alignment: Alignment.topCenter,
+          children: <Widget>[
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height >= 775.0
+                  ? MediaQuery.of(context).size.height
+                  : 775.0,
+              child: Visibility(
+                visible: _controller != null,
+                child: FutureBuilder(
+                  future: _initializeVideoPlayerFuture,
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.done) {
+                      // If the VideoPlayerController has finished initialization, use
+                      // the data it provides to limit the aspect ratio of the video.
+                      return AspectRatio(
+                        aspectRatio: _controller.value.aspectRatio,
+                        // Use the VideoPlayer widget to display the video.
+                        child: VideoPlayer(_controller),
+                      );
+                    } else {
+                      // If the VideoPlayerController is still initializing, show a
+                      // loading spinner.
+                      return Center(child: CircularProgressIndicator());
+                    }
+                  },
+                ),
+              ),
+            ),
+            GestureDetector(
+              onPanUpdate: (details) {
+                setState(
+                  () {
+                    endPoint = details.globalPosition;
+                    RenderBox renderBox = context.findRenderObject();
+                    points.add(
+                      DrawingPoints(
+                        points: renderBox.globalToLocal(details.globalPosition),
+                        paint: Paint()
+                          ..strokeCap = strokeCap
+                          ..isAntiAlias = true
+                          ..color = selectedColor.withOpacity(opacity)
+                          ..strokeWidth = strokeWidth,
+                      ),
+                    );
+                  },
+                );
+              },
+              onPanStart: (details) {
+                setState(
+                  () {
+                    RenderBox renderBox = context.findRenderObject();
+                    startPoint = details.globalPosition;
+
+                    points.add(
+                      DrawingPoints(
+                        points: renderBox.globalToLocal(details.globalPosition),
+                        paint: Paint()
+                          ..strokeCap = strokeCap
+                          ..isAntiAlias = true
+                          ..color = selectedColor.withOpacity(opacity)
+                          ..strokeWidth = strokeWidth,
+                      ),
+                    );
+                  },
+                );
+              },
+              onPanEnd: (details) {
+                setState(
+                  () {
+                    points.add(null);
+                    //_rePaint(startPoint, endPoint);
+                  },
+                );
+              },
+              child: CustomPaint(
+                size: Size.infinite,
+                isComplex: true,
+                painter: DrawingPainter(
+                  pointsList: points,
+                  sP: startPoint,
+                  eP: endPoint
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: Stack(
+        children: <Widget>[
+          Align(
+            alignment: Alignment.bottomLeft,
+            child: _controller == null
+                ? null
+                : FloatingActionButton(
+                    onPressed: () {
+                      setState(
+                        () {
+                          if (_controller.value.isPlaying) {
+                            _controller.pause();
+                          } else {
+                            _controller.play();
+                          }
+                        },
+                      );
+                    },
+                    child: Icon(
+                      _controller.value.isPlaying
+                          ? Icons.pause
+                          : Icons.play_arrow,
+                    ),
+                  ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: FloatingActionButton(
+              onPressed: () {
+                getVideo();
+              },
+              child: Icon(
+                Icons.video_call,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  getColorList() {
+    List<Widget> listWidget = List();
+    for (Color color in colors) {
+      listWidget.add(colorCircle(color));
+    }
+    Widget colorPicker = GestureDetector(
+      onTap: () {
+        showDialog(
+          context: context,
+          child: AlertDialog(
+            title: const Text('Pick a color!'),
+            content: SingleChildScrollView(
+              child: ColorPicker(
+                pickerColor: pickerColor,
+                onColorChanged: (color) {
+                  pickerColor = color;
+                },
+                //enableLabel: true,
+                pickerAreaHeightPercent: 0.8,
+              ),
+            ),
+            actions: <Widget>[
+              FlatButton(
+                child: const Text('Save'),
+                onPressed: () {
+                  setState(() => selectedColor = pickerColor);
+                  Navigator.of(context).pop();
+                },
+              ),
+            ],
+          ),
+        );
+      },
+      child: ClipOval(
+        child: Container(
+          padding: const EdgeInsets.only(bottom: 16.0),
+          height: 36,
+          width: 36,
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
+            colors: [Colors.red, Colors.green, Colors.blue],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          )),
+        ),
+      ),
+    );
+    listWidget.add(colorPicker);
+    return listWidget;
+  }
+
+  Widget colorCircle(Color color) {
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          selectedColor = color;
+        });
+      },
+      child: ClipOval(
+        child: Container(
+          padding: const EdgeInsets.only(bottom: 16.0),
+          height: 36,
+          width: 36,
+          color: color,
+        ),
+      ),
+    );
+  }
+
+  Future getVideo() async {
+    Future<File> _videoFile =
+        ImagePicker.pickVideo(source: ImageSource.gallery);
+    _videoFile.then((file) async {
+      setState(() {
+        videoFile = file;
+        _controller = VideoPlayerController.file(videoFile);
+
+        // Initialize the controller and store the Future for later use.
+        _initializeVideoPlayerFuture = _controller.initialize();
+
+        // Use the controller to loop the video.
+        _controller.setLooping(true);
+      });
+    });
+  }
+}
+
+class DrawingPainter extends CustomPainter {
+  DrawingPainter({this.pointsList, this.sP, this.eP});
+  List<DrawingPoints> pointsList;
+  List<Offset> offsetPoints = List();
+  Offset sP, eP;
+  @override
+  void paint(Canvas canvas, Size size) {
+    for (int i = 0; i < pointsList.length - 1; i++) {
+      if (pointsList[i] != null && pointsList[i + 1] != null) {
+        canvas.drawLine(pointsList[i].points, pointsList[i + 1].points,
+            pointsList[i].paint);
+      } else if (pointsList[i] != null && pointsList[i + 1] == null) {
+        offsetPoints.clear();
+        offsetPoints.add(pointsList[i].points);
+        offsetPoints.add(Offset(
+            pointsList[i].points.dx + 0.1, pointsList[i].points.dy + 0.1));
+        canvas.drawPoints(PointMode.points, offsetPoints, pointsList[i].paint);
+      }
+    }
+  }
+
+  // void paint(Canvas canvas, Size size) {
+  //   final p1 = sP;
+  //   final p2 = eP;
+  //   final paint = Paint()
+  //     ..color = Colors.black
+  //     ..strokeWidth = 4;
+  //   canvas.drawLine(p1, p2, pointsList[i].paint);
+  // }
+
+  @override
+  bool shouldRepaint(DrawingPainter oldDelegate) => true;
+}
+
+class DrawingPoints {
+  Paint paint;
+  Offset points;
+  DrawingPoints({this.points, this.paint});
+}
+
+enum SelectedMode { StrokeWidth, Opacity, Color }
